ba56ea16e09b562a60bfaa3f583206d853164ba7
Joseph Noblett <noblettjoe@gmail.com>
2023-10-19 20:36:00 -0500

    Connected the Movie reducer

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,33 +2,44 @@ import React from 'react';
 
 import MovieListItem from './MovieListItem';
 import MovieFooter from './MovieFooter';
+import { connect } from 'react-redux';
 
-const MovieList = (props)=> {
+const MovieList = (props) => {
     const movies = [];
 
     return (
         <div className="col">
             <table className="table table-striped table-hover">
                 <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Director</th>
-                    <th>Genre</th>
-                    <th>Metascore</th>
-                    <th></th>
-                </tr>
+                    <tr>
+                        <th>{props.title}</th>
+                        <th>{props.Director}</th>
+                        <th>{props.Genre}</th>
+                        <th>{props.Metascore}</th>
+                        <th></th>
+                    </tr>
                 </thead>
 
                 <tbody>
                     {
-                        movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
+                        movies.map(movie => <MovieListItem key={movie.id} movie={movie} />)
                     }
                 </tbody>
             </table>
-            
-            <MovieFooter totalMovies={movies.length}/>
+
+            <MovieFooter totalMovies={movies.length} />
         </div>
     );
 }
 
-export default MovieList;
+const mapStateToProps = state => {
+    return {
+        title: state.title,
+        director: state.director,
+        metascore: state.metascore,
+        genre: state.genre,
+        description: state.description,
+    }
+}
+
+export default connect(mapStateToProps,)(MovieList);
--- src/index.js
+++ src/index.js
@@ -9,9 +9,13 @@ import App from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 ReactDOM.render(
   <Router>
+    <Provider store={store}>
       <App />
+    </Provider>
   </Router>,
   document.getElementById('root')
 );
