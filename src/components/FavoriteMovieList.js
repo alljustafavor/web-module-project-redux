13064076454eccd194f0d9ae2b6babc157d25fee
Joseph Noblett <noblettjoe@gmail.com>
2023-10-19 23:56:40 -0500

    deleteFavorite works// all tasks are complete

--- src/components/FavoriteMovieList.js
+++ src/components/FavoriteMovieList.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { removeFavorite } from '../actions/favoritesActions';
 
 
 const FavoriteMovieList = (props) => {
-    const { favorites } = props;
+    const { favorites, removeFavorite } = props;
 
     return (<div className="col-xs savedContainer">
         <h5>Favorite Movies</h5>
@@ -13,7 +14,7 @@ const FavoriteMovieList = (props) => {
                 return <div key={movie.id}>
                     <Link className="btn btn-light savedButton" to={`/movies/${movie.id}`}>
                         {movie.title}
-                        <span><span class="material-icons">remove_circle</span></span>
+                        <span onClick={() => removeFavorite(movie.id)}><span class="material-icons">remove_circle</span></span>
                     </Link>
                 </div>
             })
@@ -26,4 +27,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps)(FavoriteMovieList);
+export default connect(mapStateToProps, { removeFavorite })(FavoriteMovieList);
